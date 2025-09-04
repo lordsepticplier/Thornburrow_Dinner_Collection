f482b3e5933659a050f3f217de86a57d1c10230d
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2025-09-04 20:50:16 +1200

    Add files via upload
    hope this works

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkexDND6fZpFJ2UPv8T4_xiEkFxzjUBr8",
+  authDomain: "thornburrow-dinner-collection.firebaseapp.com",
+  projectId: "thornburrow-dinner-collection",
+  storageBucket: "thornburrow-dinner-collection.firebasestorage.app",
+  messagingSenderId: "1049823886151",
+  appId: "1:1049823886151:web:c87b8f461952f63f13ea62"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
